4dd3ee2dc0c2d8898d11bc3a8db80f2c4beefeef
Keri Marie <kerimarie@keringtons.attlocal.net>
2017-06-13 13:40:25 -0500

    Updating homepage of website. also edited nav bar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,22 +7,22 @@
 
   <link rel="shortcut icon" type="image/x-icon" href="/img/cutlery.ico" />
 
-	<title>@yield('title')Cooking on the Fly</title>
+	<title>@yield('title')Madeline Dick Artist</title>
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-  		{{-- CUSTOM CSS --}}
+  		<!-- CUSTOM CSS -->
 		<link rel="stylesheet" type="text/css" href="/css/master.css">
 
-		<link href="https://fonts.googleapis.com/css?family=BenchNine:300|Sofia" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Advent+Pro:200|Assistant:200|Lakki+Reddy|Rajdhani:300" rel="stylesheet">
  
 		@yield('custom_css')
 
 </head>
 <body>
 	@yield('navbar')
-	{{-- @include('partials.header') --}}
+	<!-- @include('partials.header') -->
 
 	<div class="content_container">
 			@yield('content')
@@ -33,20 +33,20 @@
 			@include('partials.footer')
 		</p>
 	</div>
-	{{-- jQuery --}}
+	<!-- jQuery -->
 	<script
 		src="https://code.jquery.com/jquery-3.2.1.min.js"
 		integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
 		crossorigin="anonymous">
 	</script>
-	{{-- Bootstrap JS --}}
+	<!-- Bootstrap JS -->
 	<script
 		src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 		integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 		crossorigin="anonymous">
 	</script>
-	{{-- Custom JS (master) --}}
-	<script type="text/javascript" src="/js/master.js"></script>
+	<!-- Custom JS (master) -->
+	<!-- <script type="text/javascript" src="/js/master.js"></script> -->
 	@yield('script')
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,43 +4,33 @@
 @stop
 @section('content')
 
+
 <section class="row hp_one">
-    <div class="one" id="logo_hp"><br>
-        <img src="/img/logo1.png" class="col-xs-5" id="hp_logo">
-    </div>
+	<div class="intro">
+		<h1>MADELINE DICK</h1>
+		<h3 class="aanda">artist & artisan</h3>
+	</div>
 </section>
 
+
 <section class="row" id="hp_two">
-    <div class="col-xl-12 col-md-12 col-sm-12 col-xs-12" class="two">
-        <div class="hp_two_text_box">
-            <div class="hp_two_title">
-                <h2 class="about_COTF">It's like calling a chef...</h2>
-            </div>
+	<div class="artistartisan">
+		
+		<div class="artist_box">
+			<img class="artist_photo" src="/img/artist.jpg">
+		</div>
 
-            <div id="hp_two_text">
-                <p class="col-xs-11 col-xs-offset-1 hp_two_paragraph"><text class="important_text">Without having to make the call!</text> &nbsp; Just tell us what you have on hand, and Cooking on the Fly will do the work for you! We'll match you to recipes that you can make with ingredients you already have, so you don't have to worry or stress over your next meal!</p>
-                    
-                <p class="col-xs-11 col-xs-offset-1 hp_two_paragraph">To get started, sign in or sign up, and tell us what ingredients you have. Then, we'll show you what you can make with what you have, what you can make if you borrow something from a neighbor, and what you can make if you're willing to go to the store!</p>
-                    </div>
-                </div>
-            </div>
-        </section>
+		<div class="artisan_box">
+			<img class="artisan_photo" src="/img/artisan.jpg">
+			artisan
+		</div>
 
-        <section class="row hp_three">re
-            <div class="three">
-                <section class="text-center section_buttons">
-                    <div class="col-sm-6 buttons">
-                        <a id="sign_up_button" href="/signup/">
-                            <img height="300px" width="290px" src="/img/signup.png">
-                        </a>
-                    </div>
-                    <div class="col-sm-6">
-                        <a id="sign_in_button" href="/signin/">
-                            <img height="300px" width="290px" src="/img/signin.png">
-                        </a>
-                    </div>
-                </section>
-            </div>
-        </section>
+	</div>
+</section>
 
-        @stop
+
+<section class="row hp_three">re
+			
+</section>
+
+@stop
